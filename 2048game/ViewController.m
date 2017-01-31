b65881631cea565948de2063eb3fc9800b7addb4
米拉 <mila@komeheshages-MacBook-Air.local>
2017-01-31 14:49:40 -0500

    Initial Commit

---
+++ 2048game/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  2048game
+//
+//  Created by 米拉 on 2017/1/31.
+//  Copyright © 2017年 米拉. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
